0454092dbe966c4bb13c205270324a8308a28b41
zjun <565061763@qq.com>
2024-07-17 21:43:29 +0800

    simplify

--- lib/src/main/java/com/sd/lib/compose/paging/LazyPagingItems.kt
+++ lib/src/main/java/com/sd/lib/compose/paging/LazyPagingItems.kt
@@ -1,8 +1,6 @@
 package com.sd.lib.compose.paging
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
@@ -15,23 +13,10 @@ fun LazyPagingItems<*>.fIsRefreshing(): Boolean {
 }
 
 /**
- * 数据是否为空
- */
-@Composable
-fun LazyPagingItems<*>.fIsEmpty(): Boolean {
-    val items = this
-    return remember(items) {
-        derivedStateOf { items.itemCount == 0 }
-    }.value
-}
-
-/**
  * [CombinedLoadStates.refresh]状态
  */
 @Composable
 inline fun LazyPagingItems<*>.FUIStateRefresh(
-    /** 是否检查数据为空 */
-    checkEmpty: Boolean = true,
     /** 加载中 */
     stateLoading: @Composable () -> Unit = {},
     /** 加载错误 */
@@ -39,13 +24,12 @@ inline fun LazyPagingItems<*>.FUIStateRefresh(
     /** 无数据 */
     stateNoData: @Composable () -> Unit = {},
 ) {
-    if (checkEmpty && !fIsEmpty()) {
-        return
-    }
-    when (val loadState = loadState.refresh) {
-        is LoadState.Loading -> stateLoading()
-        is LoadState.Error -> stateError(loadState.error)
-        is LoadState.NotLoading -> stateNoData()
+    if (itemCount == 0) {
+        when (val loadState = loadState.refresh) {
+            is LoadState.Loading -> stateLoading()
+            is LoadState.Error -> stateError(loadState.error)
+            is LoadState.NotLoading -> stateNoData()
+        }
     }
 }
 
