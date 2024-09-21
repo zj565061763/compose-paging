9efc3d629867afdd35659232c8758042fdec7597
zjun <565061763@qq.com>
2024-09-22 00:40:12 +0800

    1.0.0-alpha05

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-paging"
-val libVersion = "1.0.0-alpha03"
+val libVersion = "1.0.0-alpha05"
 
 android {
    namespace = "com.sd.lib.compose.paging"
