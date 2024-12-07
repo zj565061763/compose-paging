2cee97f9cc1ebd4f8ae6d8aae0fa75112fc0299f
zjun <565061763@qq.com>
2024-12-08 01:35:38 +0800

    refactor

--- lib/src/main/java/com/sd/lib/compose/paging/PagingDataModifier.kt
+++ lib/src/main/java/com/sd/lib/compose/paging/PagingDataModifier.kt
@@ -10,84 +10,98 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 
 fun <T : Any> Flow<PagingData<T>>.modifier(
-   onEach: FPagingDataModifier<T>.(PagingData<T>) -> Unit = {
-      clearRemove()
-      clearUpdate()
-   },
-   getID: (T) -> Any,
+  onEach: FPagingDataModifier<T>.(PagingData<T>) -> Unit = { clearModify() },
+  getID: (T) -> Any,
 ): FPagingDataModifier<T> {
-   return FPagingDataModifier(
-      flow = this,
-      onEach = onEach,
-      getID = getID,
-   )
+  return FPagingDataModifier(
+    flow = this,
+    onEach = onEach,
+    getID = getID,
+  )
 }
 
 class FPagingDataModifier<T : Any> internal constructor(
-   flow: Flow<PagingData<T>>,
-   private val onEach: FPagingDataModifier<T>.(PagingData<T>) -> Unit,
-   private val getID: (T) -> Any,
+  flow: Flow<PagingData<T>>,
+  private val onEach: FPagingDataModifier<T>.(PagingData<T>) -> Unit,
+  private val getID: (T) -> Any,
 ) {
-   private val _removeFlow = MutableStateFlow<Set<Any>>(emptySet())
-   private val _updateFlow = MutableStateFlow<Map<Any, T>>(emptyMap())
+  private val _stateFlow = MutableStateFlow<State<T>>(State())
 
-   /** 数据流 */
-   val flow = flow
-      .onEach { onEach(it) }
-      .modify()
+  /** 数据流 */
+  val flow = flow
+    .onEach { onEach(it) }
+    .modify()
 
-   /**
-    * 移除ID为[id]的项
-    */
-   fun remove(id: Any) {
-      _removeFlow.update { value ->
-         if (value.contains(id)) {
-            value
-         } else {
-            value + id
-         }
+  /** 移除ID为[id]的项 */
+  fun remove(id: Any) {
+    _stateFlow.update { state ->
+      state.copy(
+        remove = state.remove.add(id),
+        update = state.update.removeWithID(id),
+      )
+    }
+  }
+
+  /** 更新项 */
+  fun update(item: T) {
+    _stateFlow.update { state ->
+      val id = getID(item)
+      when {
+        state.remove.containsID(id) -> state
+        else -> state.copy(
+          update = state.update.add(id, item)
+        )
       }
-   }
+    }
+  }
 
-   /**
-    * 清空所有删除的项
-    */
-   fun clearRemove() {
-      _removeFlow.update { emptySet() }
-   }
+  /** 清除修改 */
+  fun clearModify() {
+    _stateFlow.update { State() }
+  }
 
-   /**
-    * 更新项
-    */
-   fun update(item: T) {
-      _updateFlow.update { value ->
-         val id = getID(item)
-         if (value[id] == item) {
-            value
-         } else {
-            value + (id to item)
-         }
+  private fun Flow<PagingData<T>>.modify(): Flow<PagingData<T>> {
+    return combine(_stateFlow) { data, state ->
+      data
+        .let { state.remove.transform(it, getID) }
+        .let { state.update.transform(it, getID) }
+    }
+  }
+
+  private data class State<T : Any>(
+    val remove: RemoveState<T> = RemoveState(),
+    val update: UpdateState<T> = UpdateState(),
+  )
+
+  private data class RemoveState<T : Any>(
+    private val holder: Set<Any> = emptySet(),
+  ) {
+    fun add(id: Any): RemoveState<T> = copy(holder = holder + id)
+    fun containsID(id: Any): Boolean = holder.contains(id)
+    fun transform(
+      data: PagingData<T>,
+      getID: (T) -> Any,
+    ): PagingData<T> {
+      if (holder.isEmpty()) return data
+      return data.filter { item ->
+        !holder.contains(getID(item))
       }
-   }
+    }
+  }
 
-   /**
-    * 清空所有更新的项
-    */
-   fun clearUpdate() {
-      _updateFlow.update { emptyMap() }
-   }
-
-   private fun Flow<PagingData<T>>.modify(): Flow<PagingData<T>> {
-      return combine(_removeFlow) { data, holder ->
-         data.takeIf { holder.isEmpty() }
-            ?: data.filter { item ->
-               !holder.contains(getID(item))
-            }
-      }.combine(_updateFlow) { data, holder ->
-         data.takeIf { holder.isEmpty() }
-            ?: data.map { item ->
-               holder[getID(item)] ?: item
-            }
+  private data class UpdateState<T : Any>(
+    private val holder: Map<Any, T> = emptyMap(),
+  ) {
+    fun add(id: Any, item: T): UpdateState<T> = copy(holder = holder + (id to item))
+    fun removeWithID(id: Any): UpdateState<T> = if (holder.containsKey(id)) copy(holder = holder - id) else this
+    fun transform(
+      data: PagingData<T>,
+      getID: (T) -> Any,
+    ): PagingData<T> {
+      if (holder.isEmpty()) return data
+      return data.map { item ->
+        holder[getID(item)] ?: item
       }
-   }
+    }
+  }
 }
